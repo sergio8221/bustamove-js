54f0df7b5d5aaddeb562f3bc2f7eb853bbba49d0
Sergio Gutiérrez <sergio_gutierrez_1@hotmail.com>
2018-10-08 12:46:51 +0200

    Refactoring and cleanup

--- MainLoop.js
+++ MainLoop.js
@@ -1,69 +1,85 @@
-
 var delta = 0; //Time since last frame
 var lastFrameTimeMs = 0; // Last frame time since whenever
 
 var shootBall = false; //Shoot ball on next frame?
 
 //Flecha
-var flecha = document.getElementById('arrow');
-var rotFlecha = 0; // Ángulo de la flecha
-var dirFlecha = 0; // Dirección de rotación
-var velFlecha = 0.5; // Velocidad de rotación
-var rotaFlecha = false; // Rotar en el siguiente frame
+var arrow = {
+    element: document.getElementById('arrow'),
+    angle: 0, // Ángulo de la flecha
+    moveDirection: 0, // Dirección de rotación -> 0:left, 1:right
+    moveSpeed: 0.5, // Velocidad de rotación
+    moveNext: false, // Rotar en el siguiente frame
+    update: function () {
+        if (this.moveNext) {
+            switch (this.moveDirection) {
+                case 0:
+                    this.angle -= this.moveSpeed * delta;
+                    break;
+                case 1:
+                    this.angle += this.moveSpeed * delta;
+                    break;
+            }
+        }
+
+        if (this.angle >= 45) {
+            this.angle = 45;
+        } else if (this.angle <= -45) {
+            this.angle = -45;
+        }
+
+        this.moveNext = false;
+    },
+    draw: function () {
+        this.element.style.transform = "translateX(-50%) rotateZ(" + arrow.angle + "deg)";
+    }
+};
+
 
 //Bola
-var ball = document.getElementById('ball');
-var ballX = 50; // Ball's position
-var ballY = 50;
-var ballSpeed = 0.5; // Ball's Speed
-var ballDirection = 0; //Ball's direction
-var moveBall = false; //Move on next frame?
+var ball = {
+    element: document.getElementById('ball'),
+    x: 50, // Ball's position
+    y: 50,
+    speed: 0.5, // Ball's Speed
+    direction: 0, //Ball's direction
+    moveNext: false, //Move on next frame?
+    update: function () {
+
+    },
+    draw: function () {
+        this.element.style.left = ball.x + 'px';
+        this.element.style.top = ball.y + 'px';
+    }
+};
+
 
 window.addEventListener('keydown', function (event) {
     switch (event.key) {
         case "ArrowLeft":
-            dirFlecha = 0;
-            rotaFlecha = true;
+            arrow.moveDirection = 0;
+            arrow.moveNext = true;
             break;
         case "ArrowRight":
-            dirFlecha = 1;
-            rotaFlecha = true;
+            arrow.moveDirection = 1;
+            arrow.moveNext = true;
             break;
         case "Space":
             shootBall = true;
             break;
-
     }
-})
+});
 
 // Start things off
 requestAnimationFrame(mainLoop);
 
 function draw() {
-    ball.style.left = ballX + 'px';
-    ball.style.top = ballY + 'px';
-    arrow.style.transform = "translateX(-50%) rotateZ(" + rotFlecha + "deg)";
+    ball.draw();
+    arrow.draw();
 }
 
 function update(delta) {
-    if (rotaFlecha) {
-        switch (dirFlecha) {
-            case 0:
-                rotFlecha -= velFlecha * delta;
-                break;
-            case 1:
-                rotFlecha += velFlecha * delta;
-                break;
-        }
-    }
-
-    if (rotFlecha >= 45) {
-        rotFlecha = 45;
-    } else if (rotFlecha <= -45) {
-        rotFlecha = -45;
-    }
-
-    rotaFlecha = false;
+    arrow.update();
 }
 
 function mainLoop(timestamp) { //timestamp se lo pasa requestAnimationFrame
